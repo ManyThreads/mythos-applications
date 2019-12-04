9d2a9a5a085c6c6e1119bba577212149c1510ebb
Philipp Gypser <gypsephi@b-tu.de>
2019-12-04 11:22:00 +0100

    benchmarking

--- applications/hipsmixer/runHips.sh
+++ applications/hipsmixer/runHips.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd HiPSmixer
-make purge && make aN=X tN=X pN=ODT*
-run/ODTLES-IMEX-3R-Serial-Channel.x
+#make purge && make -j `nproc` aN=X tN=X pN=ODT* NTHREADS=2 openmp
+make purge && make -j `nproc` aN=X tN=X pN=ODT*
+numactl -C 1 run/ODTLES-IMEX-3R-Serial-Channel.x
 cd -
