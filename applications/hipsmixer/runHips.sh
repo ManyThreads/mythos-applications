ee14467fcdfed84333938b281dfeb1377dad478c
Philipp Gypser <gypsephi@b-tu.de>
2019-12-09 10:51:39 +0100

    update submodules

--- applications/hipsmixer/runHips.sh
+++ applications/hipsmixer/runHips.sh
@@ -2,6 +2,7 @@
 
 cd HiPSmixer
 #make purge && make -j `nproc` aN=X tN=X pN=ODT* NTHREADS=2 openmp
-make purge && make -j `nproc` aN=X tN=X pN=ODT*
-numactl -C 1 run/ODTLES-IMEX-3R-Serial-Channel.x
+make -f ../makefile.linux purge
+make -f ../makefile.linux -j `nproc` aN=X tN=X pN=ODT*
+run/ODTLES-IMEX-3R-Serial-Channel.x
 cd -
