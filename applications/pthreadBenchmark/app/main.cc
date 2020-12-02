74c92bd1bb2bb8661fc8d252211a1396eb2dbd4b
Philipp Gypser <gypsephi@b-tu.de>
2020-12-02 13:10:27 +0100

    pthread benchmark

--- applications/pthreadBenchmark/app/main.cc
+++ applications/pthreadBenchmark/app/main.cc
@@ -3,18 +3,52 @@
 #include <pthread.h>
 #include <atomic>
 
+#ifndef BENCH_ITERATIONS
+#define BENCH_ITERATIONS 1
+#endif
+
+void* threadBenchCreate(void* arg){
+  std::cout << "Thread says hello" << std::endl;
+  return 0;
+}
+
+void bench_pthread_create(){
+  std::cout << "Test Pthread Create" << std::endl;
+ 
+  timeval start, create;
+  
+  asm volatile ("":::"memory");
+  gettimeofday(&start, 0);
+	asm volatile ("":::"memory");
+
+	pthread_t p;
+	auto tmp = pthread_create(&p, NULL, &threadBenchCreate, (void*) 0xBEEF);
+
+  asm volatile ("":::"memory");
+  gettimeofday(&create, 0);
+	asm volatile ("":::"memory");
+
+  if(tmp == 0){
+    pthread_join(p, NULL);
+    uint64_t time =(create.tv_usec - start.tv_usec) + (create.tv_sec - start.tv_sec)*1000000;
+    std::cout << "PthreadCreate " << time << " us" << std::endl; 
+  }else{
+    std::cout << "Error: pthread_create failed!!!" << std::endl;
+  }
+  std::cout << "End Test Pthreads" << std::endl;
+}
+
 std::atomic<bool> started;
 
-void* threadBench(void* arg){
-  //std::cout << "Thread says hello" << std::endl;
+void* threadBenchResponse(void* arg){
   started.store(true);
   return 0;
 }
 
-void bench_pthreads(){
+void bench_pthread_response(){
   std::cout << "Test Pthreads" << std::endl;
  
-  timeval start, create, running, join;
+  timeval start, response;
   started.store(false);
   
   asm volatile ("":::"memory");
@@ -22,74 +56,69 @@ void bench_pthreads(){
 	asm volatile ("":::"memory");
 
 	pthread_t p;
-	auto tmp = pthread_create(&p, NULL, &threadBench, (void*) 0xBEEF);
+	auto tmp = pthread_create(&p, NULL, &threadBenchResponse, (void*) 0xBEEF);
 
-  asm volatile ("":::"memory");
-  gettimeofday(&create, 0);
-	asm volatile ("":::"memory");
+  if(tmp == 0){
+    while(!started.load());
+    
+    asm volatile ("":::"memory");
+    gettimeofday(&response, 0);
+    asm volatile ("":::"memory");
 
-  while(!started.load());
-
-  asm volatile ("":::"memory");
-  gettimeofday(&running, 0);
-	asm volatile ("":::"memory");
-  
-	pthread_join(p, NULL);
-
-  asm volatile ("":::"memory");
-  gettimeofday(&join, 0);
-	asm volatile ("":::"memory");
-
-  double cseconds =(create.tv_usec - start.tv_usec)/1000000.0 + create.tv_sec - start.tv_sec;
-  double jseconds =(join.tv_usec - start.tv_usec)/1000000.0 + join.tv_sec - start.tv_sec;
-  double rseconds =(running.tv_usec - start.tv_usec)/1000000.0 + running.tv_sec - start.tv_sec;
-
-  std::cout << "creation: " << cseconds << "s running: " << rseconds << "s join: "<< jseconds << "s" << std::endl; 
+    pthread_join(p, NULL);
+    uint64_t time =(response.tv_usec - start.tv_usec) + (response.tv_sec - start.tv_sec)*1000000;
+    std::cout << "PthreadResponse " << time << " us" << std::endl; 
+  }else{
+    std::cout << "Error: pthread_create failed!!!" << std::endl;
+  }
   std::cout << "End Test Pthreads" << std::endl;
 }
 
+void* threadBenchCreateJoin(void* arg){
+  return 0;
+}
 
-void bench_pthreads_loop(unsigned count){
-  std::cout << "Test Pthreads loop" << std::endl;
+void bench_pthread_create_join(){
+  std::cout << "Test Pthreads" << std::endl;
  
-  timeval start, create, running, join;
+  timeval start, join;
+  started.store(false);
   
   asm volatile ("":::"memory");
   gettimeofday(&start, 0);
 	asm volatile ("":::"memory");
 
-  for(unsigned i = 0; i < count; i++){
-    //std::cout << "Loop " << i << std::endl;
-    started.store(false);
+	pthread_t p;
+	auto tmp = pthread_create(&p, NULL, &threadBenchCreateJoin, (void*) 0xBEEF);
 
-    pthread_t p;
-    auto tmp = pthread_create(&p, NULL, &threadBench, (void*) 0xBEEF);
-    if(!tmp){ 
-      while(!started.load());
-      pthread_join(p, NULL);
-    }
+  if(tmp == 0){
+    pthread_join(p, NULL);
+    asm volatile ("":::"memory");
+    gettimeofday(&join, 0);
+    asm volatile ("":::"memory");
+    uint64_t time =(join.tv_usec - start.tv_usec) + (join.tv_sec - start.tv_sec)*1000000;
+    std::cout << "PthreadJoin " << time << " us" << std::endl; 
+  }else{
+    std::cout << "Error: pthread_create failed!!!" << std::endl;
   }
-
-  asm volatile ("":::"memory");
-  gettimeofday(&join, 0);
-	asm volatile ("":::"memory");
-
-  double jseconds =(join.tv_usec - start.tv_usec)/1000000.0 + join.tv_sec - start.tv_sec;
-
-  std::cout << "Pthread_loop (" << count << " iterations) finished after "<< jseconds << "s" << std::endl; 
-  std::cout << "End Test Pthreads_loop" << std::endl;
+  std::cout << "End Test Pthreads" << std::endl;
 }
 
 int main(){
   std::cout << "Main started" << std::endl;
-  
-  for(int i=0; i < 8; i++){
-    bench_pthreads();
+
+  for(int i=0; i < BENCH_ITERATIONS; i++){
+    bench_pthread_create();
   }
 
-  for(int i=0; i < 2; i++){
-    bench_pthreads_loop(1000);
+  for(int i=0; i < BENCH_ITERATIONS; i++){
+    bench_pthread_response();
   }
 
+  for(int i=0; i < BENCH_ITERATIONS; i++){
+    bench_pthread_create_join();
+  }
+
+  std::cout << "Main finished" << std::endl;
   return 0;
 };
---
+++ benchmarks/bench_PM_Pthread.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+nr_cpus=`lscpu --parse | awk -F"," '{if ($4 == 0) print $4}' | wc -l`
+echo $nr_cpus
+singlecpu=`lscpu --parse | awk -F"," '{if ($4 == 0) print $1}' | tail -n 1 | xargs echo -n | sed 's/ /,/g'`
+echo $singlecpu
+
+#config
+SUMMARYFILE="bench_PM_summary.out"
+ITERATIONS=100
+NUMTHREADS=(2)
+
+for i in ${NUMTHREADS[*]}
+do
+	if [ $i -ge $nr_cpus ]
+	then
+		echo "Error: too few cores(${nr_cpus} cores available on numa node 0, ${i} cores requested)"
+		exit
+	fi
+done
+
+#periodically delay sudo timeout
+sudo echo "sudo sudo"
+#while :; do sudo -v; sleep 59; done &
+sudoinfiniloop=$!
+
+
+echo "Benchmarking Pthread creation Linux vs Mythos"
+echo "============================================="
+echo ""
+
+rm -f ${SCRIPTDIR}/*.out
+touch ${SCRIPTDIR}/${SUMMARYFILE}
+
+echo "Setup:"
+cd ${SCRIPTDIR}/..
+make --silent clean all > /dev/null
+cd - 
+
+########################################
+echo "Mythos IHK Benchmark Pthread:" | tee -a ${SCRIPTDIR}/${SUMMARYFILE}
+cd ${SCRIPTDIR}/../kernel-ihk
+
+echo ">> build mythos"
+make --silent clean all BENCH_APP_FLAGS="-DBENCH_ITERATIONS=${ITERATIONS}" > /dev/null
+
+usedcpus=`lscpu --parse | awk -F"," '{if ($4 == 0) print $1}' | tail -n ${NUMTHREADS} | xargs echo -n | sed 's/ /,/g'`
+echo ${usedcpus}
+
+echo ">> run mythos"
+sudo rm -rf /tmp/ihkmond
+sudo ../mythos/3rdparty/ihkreboot.sh -m 4096M -k 1 -c ${usedcpus} -p ${SCRIPTDIR}/../kernel-ihk/boot64.elf
+make wait stop | tee ${SCRIPTDIR}/run.out 
+grep "PthreadCreate" ${SCRIPTDIR}/run.out | sed 's/^.*\(PthreadCreate.*\)/\1/'| tee -a ${SCRIPTDIR}/${SUMMARYFILE} >  ${SCRIPTDIR}/Mythos_PthreadCreate.out
+grep "PthreadResponse" ${SCRIPTDIR}/run.out | sed 's/^.*\(PthreadResponse.*\)/\1/'| tee -a ${SCRIPTDIR}/${SUMMARYFILE} >  ${SCRIPTDIR}/Mythos_PthreadResponse.out
+grep "PthreadJoin" ${SCRIPTDIR}/run.out | sed 's/^.*\(PthreadJoin.*\)/\1/'| tee -a ${SCRIPTDIR}/${SUMMARYFILE} >  ${SCRIPTDIR}/Mythos_PthreadJoin.out
+cd -
+rm ${SCRIPTDIR}/run.out
+
+##########################################
+echo "Linux Benchmark Pthread:" | tee -a ${SCRIPTDIR}/${SUMMARYFILE}
+
+usedcpus=`lscpu --parse | awk -F"," '{if ($4 == 0) print $1}' | tail -n ${NUMTHREADS} | xargs echo -n | sed 's/ /,/g'`
+echo ${usedcpus}
+
+cd ${SCRIPTDIR}/../applications/pthreadBenchmark/app/
+echo ">> build ODT for linux"
+make clean all BENCH_APP_FLAGS="-DBENCH_ITERATIONS=${ITERATIONS}" > /dev/null
+
+echo ">> run ODT linux"
+numactl -C ${usedcpus} ./main | tee ${SCRIPTDIR}/run.out
+
+grep "PthreadCreate" ${SCRIPTDIR}/run.out | sed 's/^.*\(PthreadCreate.*\)/\1/'| tee -a ${SCRIPTDIR}/${SUMMARYFILE} >  ${SCRIPTDIR}/Linux_PthreadCreate.out
+grep "PthreadResponse" ${SCRIPTDIR}/run.out | sed 's/^.*\(PthreadRespone.*\)/\1/'| tee -a ${SCRIPTDIR}/${SUMMARYFILE} >  ${SCRIPTDIR}/Linux_PthreadResponse.out
+grep "PthreadJoin" ${SCRIPTDIR}/run.out | sed 's/^.*\(PthreadJoin.*\)/\1/'| tee -a ${SCRIPTDIR}/${SUMMARYFILE} >  ${SCRIPTDIR}/Linux_PthreadJoin.out
+cd -
+rm ${SCRIPTDIR}/run.out
+
+#enable sudo timeout
+#kill "$sudoinfiniloop"
+
+# plot results
+cd ${SCRIPTDIR}
+#python3 plot.py
+hostname=`hostname`
+time=`date +%y-%m-%d_%H:%M`
+outdir="PM/${hostname}_${time}"
+echo ${outdir}
+mkdir ${outdir}
+mv *.out *.pdf ${outdir}
+cd -
